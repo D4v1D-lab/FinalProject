7927b839826e1e379495bacf924a0b615e2c824e
D4v1D-lab <deividfrend@gmail.com>
2020-12-09 19:49:40 -0700

    add a model

---
+++ SacramentMeetingPlanner/SacramentMeetingPlanner/Migrations/20201209225215_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SacramentMeetingPlanner.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Speakers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Speaker = table.Column<string>(nullable: true),
+                    Subject = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speakers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfMeeting = table.Column<DateTime>(nullable: false),
+                    OpeningPrayer = table.Column<string>(nullable: false),
+                    ConductingLeader = table.Column<string>(nullable: false),
+                    OpeningHymn = table.Column<string>(nullable: false),
+                    SacramentHymn = table.Column<string>(nullable: false),
+                    ClosingHymn = table.Column<string>(nullable: false),
+                    ClosingPrayer = table.Column<string>(nullable: false),
+                    SpeakersId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Message_Speakers_SpeakersId",
+                        column: x => x.SpeakersId,
+                        principalTable: "Speakers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_SpeakersId",
+                table: "Message",
+                column: "SpeakersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+
+            migrationBuilder.DropTable(
+                name: "Speakers");
+        }
+    }
+}
